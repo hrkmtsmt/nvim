abd613bb6fc397ceac15e31437d137c45a5915cb
hrkmtsmt <hrkmtsmt@icloud.com>
2026-01-17 22:00:19 +0900

    fix: Fixed commit information order.

--- init.lua
+++ init.lua
@@ -8,6 +8,17 @@ vim.opt.pumblend = 0
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+vim.opt.autoread = true
+vim.opt.updatetime = 300
+
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  pattern = "*",
+  callback = function()
+    if vim.fn.mode() ~= "c" then
+      vim.cmd("checktime")
+    end
+  end,
+})
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
--- lua/plugins/git-blame.lua
+++ lua/plugins/git-blame.lua
@@ -3,7 +3,7 @@ return {
   config = function()
     require("gitblame").setup({
       enabled = true,
-      message_template = " <summary> • <date> • <author>",
+      message_template = "<author> | <date> | <summary>",
       date_format = "%Y-%m-%d %H:%M",
       virtual_text_column = nil,
       highlight_group = "Comment",
