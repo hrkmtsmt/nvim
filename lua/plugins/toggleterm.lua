c8924d3c331e6ed6d1c4dbcd09307bc1fc1a613f
hrkmtsmt <hrkmtsmt@icloud.com>
2026-01-17 21:37:33 +0900

    feat: Set up ToggleTerm keymaps.

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,11 +1,20 @@
 return {
   "akinsho/toggleterm.nvim",
   version = "*",
-  opts = {
-    size = function()
-      return math.floor(vim.o.columns * 0.5)
-    end,
-    open_mapping = [[<D-S-t>]],
-    direction = "vertical",
-  },
+  config = function()
+    require("toggleterm").setup({
+      size = math.floor(vim.o.columns * 0.5),
+      open_mapping = [[<C-\>]],
+      direction = "vertical",
+    })
+
+    vim.keymap.set("n", "<leader>tf", "<cmd>ToggleTerm direction=float<cr>")
+    vim.keymap.set("n", "<leader>tt", function()
+      vim.cmd(string.format("ToggleTerm size=%d direction=vertical", math.floor(vim.o.columns * 0.5)))
+    end)
+    vim.keymap.set("n", "<leader>th", function()
+      vim.cmd(string.format("ToggleTerm size=%d direction=horizontal", math.floor(vim.o.lines * 0.5)))
+    end)
+  end,
 }
+
