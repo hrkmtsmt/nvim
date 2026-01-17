4318ea785ed95f2d09d542602b77fbb6121d848b
hrkmtsmt <hrkmtsmt@icloud.com>
2026-01-18 00:47:38 +0900

    refacotr: Standardize pulugin config file formats.

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -5,28 +5,30 @@ return {
     "MunifTanjim/nui.nvim",
     "rcarriga/nvim-notify",
   },
-  opts = {
-    lsp = {
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true,
-      },
-    },
-    presets = {
-      bottom_search = true,
-      command_palette = true,
-      long_message_to_split = true,
-      lsp_doc_border = true,
-      inc_rename = false,
-    },
-    views = {
-      cmdline_popup = {
-        position = {
-          row = "50%",
-          col = "50%",
+  config = function()
+    require("noice").setup({
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
         },
       },
-    },
-  },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        lsp_doc_border = true,
+        inc_rename = false,
+      },
+      views = {
+        cmdline_popup = {
+          position = {
+            row = "50%",
+            col = "50%",
+          },
+        },
+      },
+    })
+  end,
 }
