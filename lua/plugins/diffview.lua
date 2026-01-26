94a572a2317e436b1e9f142e51673fbe64df9748
hrkmtsmt <hrkmtsmt@icloud.com>
2026-01-26 23:21:10 +0900

    fix: Update DiffView commands.

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -3,8 +3,9 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     require("diffview").setup()
-    vim.keymap.set("n", "<leader>dvo", "<cmd>DiffviewOpen<cr>", { desc = "Diffview Open" })
-    vim.keymap.set("n", "<leader>dvc", "<cmd>DiffviewClose<cr>", { desc = "Diffview Close" })
-    vim.keymap.set("n", "<leader>dve", "<cmd>DiffviewFocusFiles<cr>", { desc = "Diffview Files" })
+    vim.keymap.set("n", "<leader>do", "<cmd>DiffviewOpen<cr>", { desc = "Diffview Open" })
+    vim.keymap.set("n", "<leader>dc", "<cmd>DiffviewClose<cr>", { desc = "Diffview Close" })
+    vim.keymap.set("n", "<leader>de", "<cmd>DiffviewFocusFiles<cr>", { desc = "Diffview Files" })
+    vim.keymap.set("n", "<leader>dr", "<cmd>DiffviewRefresh<cr>", { desc = "Diffview Refresh" })
   end,
 }
