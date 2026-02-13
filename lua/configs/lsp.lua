23ef523f1578467b5f6634ff43a35b76824fcb31
hrkmtsmt <hrkmtsmt@icloud.com>
2026-02-13 23:15:38 +0900

    feat: Setup Hsskell language server.

--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -1,6 +1,6 @@
 require("mason").setup()
 
-local language_servers = { "lua_ls", "ts_ls", "pyright", "terraformls", "yamlls" }
+local language_servers = { "lua_ls", "ts_ls", "pyright", "terraformls", "yamlls", "hls" }
 require("mason-lspconfig").setup({
   ensure_installed = language_servers,
 })
