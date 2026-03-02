f949fa2aad3f159e44a66aab8735024ab435ba09
hrkmtsmt <hrkmtsmt@icloud.com>
2026-03-02 23:34:02 +0900

    feat: ToggleTerm can open multiple terminal.

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -14,11 +14,17 @@ return {
     vim.keymap.set("n", "<leader>tt", function()
       vim.cmd(string.format("ToggleTerm size=%d direction=vertical", math.floor(vim.o.columns * 0.5)))
     end, { desc = "TogglerTerm Open" })
+    vim.keymap.set("n", "<leader>tn", function()
+      local terminals = require("toggleterm.terminal").get_all()
+      local new_id = #terminals + 1
+      vim.cmd(string.format("%dToggleTerm size=%d direction=vertical", new_id, math.floor(vim.o.columns * 0.5)))
+    end, { desc = "ToggleTerm New" })
     vim.keymap.set("n", "<leader>tv", function()
       vim.cmd(string.format("ToggleTerm size=%d direction=vertical", math.floor(vim.o.columns * 0.5)))
     end, { desc = "TogglerTerm Open with Vertical" })
     vim.keymap.set("n", "<leader>th", function()
       vim.cmd(string.format("ToggleTerm size=%d direction=horizontal", math.floor(vim.o.lines * 0.5)))
     end, { desc = "TogglerTerm Open with Horizontal" })
+    vim.keymap.set("n", "<leader>ts", "<cmd>TermSelect<cr>", { desc = "ToggleTerm Select" })
   end,
 }
