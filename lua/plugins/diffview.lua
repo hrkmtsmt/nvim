cb35cf6a8c6449e11dfe78df2fc7707dbeeb353d
hrkmtsmt <hrkmtsmt@icloud.com>
2026-03-03 01:34:15 +0900

    feat: Diffview can resolve conflicta and restore code.

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -5,11 +5,12 @@ return {
     local actions = require("diffview.config").actions
     require("diffview").setup()
     vim.keymap.set("n", "<leader>dvo", "<cmd>DiffviewOpen<cr>", { desc = "Diffview Open" })
-    vim.keymap.set("n", "<leader>dvc", "<cmd>DiffviewClose<cr>", { desc = "Diffview Close" })
+    vim.keymap.set("n", "<leader>dvq", "<cmd>DiffviewClose<cr>", { desc = "Diffview Close" })
     vim.keymap.set("n", "<leader>dve", "<cmd>DiffviewFocusFiles<cr>", { desc = "Diffview Files" })
     vim.keymap.set("n", "<leader>dvr", "<cmd>DiffviewRefresh<cr>", { desc = "Diffview Refresh" })
     vim.keymap.set("n", "<leader>dvf", actions.focus_entry, { desc = "Diffview Focus Entry" })
-    vim.keymap.set("n", "<leader>co", actions.conflict_choose("ours"), { desc = "Diffview Choose OURS (CURRENT)" })
-    vim.keymap.set("n", "<leader>ct", actions.conflict_choose("theirs"), { desc = "Diffview Choose THEIRS (INCOMING)" })
+    vim.keymap.set("n", "<leader>dx", actions.restore_entry, { desc = "Diffview Restore Entry" })
+    vim.keymap.set("n", "<leader>do", actions.conflict_choose("ours"), { desc = "Diffview Choose OURS (CURRENT)" })
+    vim.keymap.set("n", "<leader>dt", actions.conflict_choose("theirs"), { desc = "Diffview Choose THEIRS (INCOMING)" })
   end,
 }
